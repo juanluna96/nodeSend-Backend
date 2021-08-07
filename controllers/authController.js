6c7d69ed6ba82ca72178821bf531618749c2c5d6
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-06 21:45:56 -0500

    Verificando si un usuario existe o no

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,18 @@ const bcrypt = require("bcrypt");
 const { validationResult } = require("express-validator");
 
 exports.autenticarUsuario = async (req, res, next) => {
+    // Revisar si hay errores
 
+    // Buscar el usuario para ver si esta registrado
+    const { email } = req.body;
+    const usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+        return res.status(401).json({ message: 'El usuario no existe' });
+    }
+
+    console.log(`usuario`, usuario);
+    // Verificar el password y autenticar el usuario
 }
 exports.usuarioAutenticado = async (req, res) => {
 
