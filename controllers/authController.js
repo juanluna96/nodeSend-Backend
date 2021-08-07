8069970820b77d8e98ad1b5223620d584197001b
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-07 08:27:07 -0500

    Revisando si el password es correcto o no

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,15 +6,20 @@ exports.autenticarUsuario = async (req, res, next) => {
     // Revisar si hay errores
 
     // Buscar el usuario para ver si esta registrado
-    const { email } = req.body;
+    const { email, password } = req.body;
     const usuario = await Usuario.findOne({ email });
 
     if (!usuario) {
         return res.status(401).json({ message: 'El usuario no existe' });
     }
 
-    console.log(`usuario`, usuario);
     // Verificar el password y autenticar el usuario
+    if (bcrypt.compareSync(password, usuario.password)) {
+        // Crear JWT
+    } else {
+        return res.status(401).json({ message: 'Password incorrecto' });
+    }
+
 }
 exports.usuarioAutenticado = async (req, res) => {
 
