9467a48e4cb464efb0b1b63cfe43343a6268756b
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-07 09:02:39 -0500

    Creando un JSON Web Token (JWT)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const Usuario = require("../models/Usuario")
 const bcrypt = require("bcrypt");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+require('dotenv').config({ path: 'variables.env' });
 
 exports.autenticarUsuario = async (req, res, next) => {
     // Revisar si hay errores
@@ -16,6 +18,15 @@ exports.autenticarUsuario = async (req, res, next) => {
     // Verificar el password y autenticar el usuario
     if (bcrypt.compareSync(password, usuario.password)) {
         // Crear JWT
+        const token = jwt.sign({
+            id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email
+        }, process.env.SECRETA, {
+            expiresIn: '8h'
+        });
+
+        return res.json({ token });
     } else {
         return res.status(401).json({ message: 'Password incorrecto' });
     }
