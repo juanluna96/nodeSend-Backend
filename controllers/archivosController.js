28ee288641fbb3958cf64c041bf4e6eb056b9094
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-08 08:51:02 -0500

    Creando Routing y Controllers

---
+++ controllers/archivosController.js
@@ -0,0 +1,6 @@
+exports.subirArchivo = async (req, res, next) => {
+
+}
+exports.eliminarArchivo = async (req, res, next) => {
+
+}
--- index.js
+++ index.js
@@ -15,5 +15,6 @@ app.use(express.json());
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/enlaces', require('./routes/enlaces'));
+app.use('/api/archivos', require('./routes/archivos'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
