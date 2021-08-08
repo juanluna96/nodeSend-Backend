0258ae362f1076ee3b74211e80e66c413872c03d
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-08 09:47:56 -0500

    Creando una Configuración más avanzada con Multer

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,5 +1,33 @@
+// Subida de archivos
+const multer = require("multer");
+const shortid = require("shortid");
+
+const configuracionMulter = {
+    limits: { fileSize: 1024 * 1024 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, __dirname + '/../uploads')
+        },
+        filename: (req, file, cb) => {
+            const extension = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+            cb(null, `${shortid.generate()}${extension}`);
+        }
+    })
+}
+
+const upload = multer(configuracionMulter).single('archivo');
+
 exports.subirArchivo = async (req, res, next) => {
-    console.log(`req.file`, req.file);
+    upload(req, res, async (error) => {
+        console.log(req.file);
+
+        if (!error) {
+            res.json({ archivo: req.file.filename });
+        } else {
+            console.log(error);
+            return next();
+        }
+    });
 }
 
 exports.eliminarArchivo = async (req, res, next) => {
